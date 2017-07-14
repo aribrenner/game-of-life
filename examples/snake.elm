29de5f643829ecf4f51ad191bc95072d00247717
Ari Brenner <ari.brenner@generalassemb.ly>
2017-07-14 10:20:19 -0400

    init snake

---
+++ examples/snake.elm
@@ -0,0 +1,79 @@
+import Html exposing (Html, div, text)
+import Dict exposing (Dict)
+import Time exposing (Time, second)
+
+boardSize = 10
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+-- MODEL
+
+type alias Model = {board: Dict (Int, Int) Int, info: String}
+
+
+init : (Model, Cmd Msg)
+init =
+  ({board = Dict.empty, info = ""}, Cmd.none)
+
+
+fullBoard : List (List (Int, Int))
+fullBoard =
+  buildBoard (boardSize - 1)
+--
+--
+buildBoard : Int -> List (List (Int, Int))
+buildBoard cur =
+  let
+    miniBoard = [fullRow (boardSize - 1) cur]
+  in
+    if cur == 0 then miniBoard else List.append miniBoard (buildBoard (cur - 1))
+
+fullRow : Int -> Int -> List (Int, Int)
+fullRow i j =
+  let
+    pair = [(i, j)]
+  in
+    if i == 0 then pair else List.append pair (fullRow (i-1) j)
+
+
+-- UPDATE
+
+
+type Msg
+  = Tick Time
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Tick newTime ->
+      ({ model | info = toString newTime}, Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every second Tick
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [] [
+      div [] [text model.info],
+      div [] [text (toString fullBoard)]
+    ]
